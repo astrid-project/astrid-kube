8508160bfaf29b207697820f727fb9efed57f4cc
Elis Lulja <elis.lulja@gmail.com>
2019-05-08 17:18:49 +0200

    changed default action to forward

--- graph/infrastructure-handler.go
+++ graph/infrastructure-handler.go
@@ -212,6 +212,7 @@ func (handler *InfrastructureHandler) handlePod(pod *core_v1.Pod) {
 	if !utils.AttachFirewall(pod.Status.PodIP) {
 		return
 	}
+	handler.log.Infoln("Attached firewall to pod:", pod.Name)
 
 	//	TODO: look int pod.name as uid
 	handler.infoBuilder.PushInstance(pod.Labels["astrid.io/service"], pod.Status.PodIP, pod.Name)
--- utils/firewall.go
+++ utils/firewall.go
@@ -18,6 +18,30 @@ func CreateFirewall(ip string) bool {
 		log.Infoln("Could not create firewall:", err, resp)
 		return false
 	}
+
+	return changeDefaultForward(ip)
+}
+
+func changeDefaultForward(ip string) bool {
+	jsonStr := []byte(`"forward"`)
+	directions := []string{"ingress", "egress"}
+	client := http.Client{}
+
+	for _, direction := range directions {
+		req, err := http.NewRequest("PATCH", "http://"+ip+":9000"+polycubePath+firewallPath+"fw/chain/"+direction+"/default", bytes.NewBuffer(jsonStr))
+		if err != nil {
+			log.Infoln("Could not change default action in", direction, err, req)
+			return false
+		}
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Infoln("Could not change default action in", direction, err, resp)
+			return false
+		}
+		defer resp.Body.Close()
+	}
+
 	return true
 }
 
